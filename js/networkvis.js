39276b53cd38eaf09bf510e2cb0d2efdebac8d8b
Zane Wolf <30910417+zanewolf@users.noreply.github.com>
2020-11-13 22:24:49 -0500

    added tooltip and CLEANED THE COUNTRY DATA (treedata.js) - SEE IF YOU WANT TO DUPLICATE EFFORTS

--- js/networkvis.js
+++ js/networkvis.js
@@ -2,7 +2,14 @@
 * NetworkVis - Object constructor function
 * @param _parentElement 	-- the HTML element in which to draw the visualization
 * @param _data						-- the actual data
-*/
+*
+* /*
+ *
+ * author: Zane
+ * date created: 11/09/2020
+ * date last modified:
+ */
+
 
 class NetworkVis {
 
@@ -14,7 +21,7 @@ class NetworkVis {
         // data[3] is the hierarchical array
         this.treeData = this.data[3];
         this.practiceData = this.data[4];
-        console.log(this.data[4])
+        // console.log(this.data[4])
 
         this.initVis();
     }
@@ -29,7 +36,7 @@ class NetworkVis {
 
         //set up SVG drawing area
         // define margins
-        vis.margin = {top: 20, right: 50, bottom: 20, left: 20};
+        vis.margin = {top: 10, right: 20, bottom: 20, left: 20};
         vis.width = $("#" + vis.parentElement).width() - vis.margin.left - vis.margin.right;
         vis.height = $("#" + vis.parentElement).height() - vis.margin.top - vis.margin.bottom;
 
@@ -40,25 +47,26 @@ class NetworkVis {
             .append('g')
             .attr('transform', `translate (${vis.width/2}, ${vis.height/2})`);
 
-        console.log(vis.height)
+        // console.log(vis.height)
         vis.radius = vis.height/2;
 
         vis.tree = data => {
-            vis.root = d3.hierarchy(data, function(d) {
-                return d.children;
-            });
-            console.log(vis.root)
-            // vis.root.x0 =vis.height/2;
-            // vis.root.y0=vis.width/2;
+            // vis.root = d3.hierarchy(data, function(d) {
+            //     return d.children;
+            // });
+            vis.root = d3.hierarchy(data).sort((a, b) => d3.descending(a.height, b.height) || d3.ascending(a.data.name, b.data.name));
+            // console.log(vis.root)
+            vis.root.x0 =vis.height/2;
+            vis.root.y0=vis.width/2;
             vis.root.dx = 0;
             vis.root.dy = vis.width / (vis.root.height + 1);
             return d3.cluster().size([360, vis.radius-60])(vis.root);
         }
         // var root = vis.tree(vis.practiceData)
-        var root = vis.tree(vis.treeData)
+        vis.rootData = vis.tree(vis.treeData)
 
         // Features of the links between nodes:
-        var linksGenerator = d3.linkRadial()
+        vis.linksGenerator = d3.linkRadial()
             .angle(function (d) {
                 return d.x / 180 * Math.PI;
             })
@@ -67,28 +75,71 @@ class NetworkVis {
             });
 
         // Add the links between nodes:
-        vis.svg.selectAll('path')
-            .data(root.links())
-            .enter()
-            .append('path')
-            .attr("d", linksGenerator)
-            .style("fill", 'none')
-            .attr("stroke", '#ccc')
+        vis.links = vis.svg.selectAll('path')
+            .attr("class", "links");
+
 
 
         // Add a circle for each node.
-        vis.svg.selectAll("g")
-            .data(root.descendants())
-            .enter()
-            .append("g")
-            .attr("transform", function (d) {
-                return "rotate(" + (d.x - 90) + ")translate(" + d.y + ")";
-            })
-            .append("circle")
-            .attr("r", 7)
-            .style("fill", "#69b3a2")
-            .attr("stroke", "black")
-            .style("stroke-width", 2)
+        vis.circles = vis.svg.selectAll("g")
+            .attr("class", "circles");
+
+        // TODO add tooltip
+        vis.tooltip = d3.select("body").append('div')
+            .attr('class', "tooltip")
+            .attr('id', 'networkTooltip');
+
+
+
+        // add labels
+        // TODO figure out placement
+      //   vis.svg.append("g")
+      //       .attr("font-family", "sans-serif")
+      //       .attr("font-size", 10)
+      //       // .attr("stroke-linejoin", "round")
+      //       .attr("stroke-width", 0)
+      //       .attr("fill", "white")
+      //       .selectAll("text")
+      //       .data(vis.rootData.descendants())
+      //       .join("text")
+      //       .attr("transform", d => `
+      //   rotate(${d.x * 180 / Math.PI - 90})
+      //   translate(${d.y},0)
+      //   rotate(${d.x >= Math.PI ? 180 : 0})
+      // `)
+      //       .attr("dy", "0.31em")
+      //       .attr("x", d => d.x < Math.PI === !d.children ? 7 : -7)
+      //       .attr("text-anchor", d => d.x < Math.PI === !d.children ? "start" : "end")
+      //       .text(d => d.data.name)
+      //       .clone(true).lower()
+      //       .attr("stroke", "white");
+
+        // vis.svg.append("text")
+        //     .data(roots.descendants())
+        //     .attr("dy", "0.31em")
+        //     .attr("x", function(d) { return d.x < 180 === !d.children ? 6 : -6; })
+        //     .style("text-anchor", function(d) { return d.x < 180 === !d.children ? "start" : "end"; })
+        //     .attr("transform", function(d) { return "rotate(" + (d.x < 180 ? d.x - 90 : d.x + 90) + ")"; })
+        //     .text(d=>d.data.name);
+      //   vis.svg.append("g")
+      //       .attr("font-family", "sans-serif")
+      //       .attr("font-size", 12)
+      //       // .attr("stroke-linejoin", "round")
+      //       .attr("fill", "white")
+      //       .attr("stroke-width", 0)
+      //       .selectAll("text")
+      //       .data(root.descendants())
+      //       .join("text")
+      //       .attr("x", function(d) { return d.x < Math.PI === !d.children ? 6 : -10; })
+      //       .attr("transform", d => `rotate(${d.x < Math.PI ? d.x - 60 : d.x + 60}) translate(${d.y},0) rotate(${d.x >= Math.PI ? 180 : 0})
+      // `)
+      // //       .attr("transform", d => `rotate(${d.x * 180 / Math.PI}) translate(${d.y},0) rotate(${d.x >= Math.PI ? 180 : 0})
+      // // `)
+      //       .attr("dy", "0.31em")
+      //       // .attr("x", d => d.x < Math.PI === !d.children ? 6 : -6)
+      //       .attr("text-anchor", d => d.x < Math.PI === !d.children ? "start" : "end")
+      //       .text(d => d.data.name)
+      //       .clone(true).lower();
 
         // (Filter, aggregate, modify data)
         vis.wrangleData();
@@ -114,6 +165,128 @@ class NetworkVis {
 
     updateVis() {
         let vis = this;
+
+        // draw links
+        vis.links
+            .data(vis.rootData.links())
+            .enter()
+            .append('path')
+            .attr("d", vis.linksGenerator)
+            .style("fill", 'none')
+            .attr("stroke", '#ccc');
+
+        // draw circles
+        vis.circles
+            .data(vis.rootData.descendants())
+            .enter()
+            .append("g")
+            .attr("transform", function (d) {
+                return "rotate(" + (d.x - 90) + ")translate(" + d.y + ")";
+            })
+            .append("circle")
+            .attr("r", 4)
+            //need to create function for linking circle fill color to information
+            .style("fill", "#69b3a2")
+            .attr("stroke", "black")
+            .style("stroke-width", 2)
+            .on('mouseover', function(event, d){
+                // console.log(d)
+                d3.select(this)
+                    .attr('stroke-width', '2px')
+                    .attr('stroke', 'black')
+                    .attr('fill', 'grey')
+
+                vis.tooltip
+                    .style("opacity", 1)
+                    .style("left", event.pageX + 20 + "px")
+                    .style("top", event.pageY + "px");
+
+                if (d.height == 0){
+                    // rocket type tooltip
+                    vis.tooltip
+                        .html(`
+                     <div style="border: thin solid grey; border-radius: 5px; background: darkgray; padding: 20px">
+                         <p> <strong>Rocket Type: </strong>${d.data.name}</p>
+                         <p> <strong>Company: </strong>${d.data.information.company}</p>
+                         <p> <strong>Country: </strong>${d.data.information.country}</p>
+                         <p> <strong>Total Launches: </strong>${d.data.information.total}</p>
+                     </div>`);
+                } else if (d.height == 1){
+                    // company tooltip
+
+                    // calculate totals
+                    let totalRockets=0;
+                    let totalLaunches=0;
+                    let totalSuccesses=0;
+                    let totalFailures=0;
+                    d.data.children.forEach((d,i)=>{
+                        totalRockets++;
+                        totalLaunches += d.information.total;
+                        totalSuccesses += d.information.successes;
+                        totalFailures += d.information.failures;
+                    })
+                    let ratio = totalSuccesses/totalLaunches*100;
+
+                    vis.tooltip
+                        .html(`
+                     <div style="border: thin solid grey; border-radius: 5px; background: darkgray; padding: 20px">
+                         <h3>${d.data.name}<h3>
+                         <hr>
+                         <p> <strong>Total Rockets: </strong>${totalRockets}</p>
+                         <p> <strong>Success Ratio: </strong>${ratio.toFixed(1)}%</p>
+                     </div>`);
+                } else if (d.height == 2){
+
+                    // country tooltip
+                    // console.log(d)
+
+                    //calculate totals
+                    let totalRockets=0;
+                    let isActive = 'No';
+                    d.data.children.forEach((d,i)=>{
+                        // console.log(d)
+                        d.children.forEach((d,i)=>{
+                            // console.log(d)
+                            totalRockets++;
+                            if (d.information.status=="StatusActive"){
+                                isActive = "Yes";
+                            }
+
+                        })
+                    })
+
+                    vis.tooltip
+                        .html(`
+                     <div style="border: thin solid grey; border-radius: 5px; background: darkgray; padding: 20px">
+                         <h3>${d.data.name}<h3>
+                         <hr>
+                         <p> <strong>Total Rocket Types: </strong>${totalRockets}</p>
+                         <p> <strong>Currently Active?: </strong>${isActive}</p>
+                     </div>`);
+                } else if (d.height == 3){
+                    //potential easter egg. However, I get a 404 error file not found
+                    // need to find the correct way to link to images
+                    // var myImagePath = "spacemonkey.png";
+                    // // var string = '<img src= + "images/spacemonkey.png" + />`;
+                    // vis.tooltip
+                    //     .html(`<img src = +" myImagePath + />"`)
+                    //     // .html(`img src={\`${API_URL}/${myImagePath}\`}`)//this will add the image on mouseover
+                    //     .style("left", (event.pageX + 10) + "px")
+                    //     .style("top", (event.pageY + 50) + "px")
+                    //     .style("font-color", "white");
+
+                }
+            })
+            .on('mouseout', function(event, d){
+                d3.select(this)
+                    .attr('stroke-width', '0px')
+                    .attr("fill", "#69b3a2")
+                vis.tooltip
+                    .style("opacity", 0)
+                    .style("left", 0)
+                    .style("top", 0)
+                    .html(``);
+            });
 
 
     }
--- js/treedata.js
+++ js/treedata.js
@@ -3,7 +3,7 @@
  *                  for the radial dendrogram visualization
  * author: Zane
  * date created: 11/09/2020
- * date last modified: 11/10/2020
+ * date last modified: 11/13/2020
  * product: a stringified json array of the data in hierarchical form.
  *          Which was then copied and pasted into a .json file because exporting files in this framework is stupid.
  */
@@ -16,19 +16,51 @@ function loadData() {
 
         // since the country data seems a bit weird (Florida, California, and US are all independent entries)
         // extracting the actual country from the location of launch
+        var rowID=0;
         csv.forEach((d, i) => {
-            d["CountryName"] = splitWords(d.Location)
+            // d["CountryName"] = splitWords(d.Location)
+            if (d.CompanyName=="CASC"){
+                d.Country="China";
+            } else if (d.CompanyName =="SRC" | d.CompanyName == "Sea Launch"){
+                d.Country = "Russia";
+            } else if (d.Country =="Korea"){
+                d.Country = "North Korea";
+            } else if (d.CompanyName =="IRGC"){
+                d.Country = "Iran";
+            }else if (d.Country =="Florida"){
+                d.Country = "USA";
+            }else if (d.Country =="California"){
+                d.Country = "USA";
+            }else if (d.Country =="Zealand"){
+                d.Country = "New Zealand";
+            }else if (d.Country =="Zealand"){
+                d.Country = "New Zealand";
+            }else if (d.Country =="Facility"){
+                d.Country = "USA"
+            }else if (d.Country =="Canaria"){
+                d.Country = "USA"
+            }else if (d.CompanyName =="Kosmotras"| d.CompanyName =="Land Launch"|d.CompanyName =="OKB-586"|d.CompanyName =="Roscosmos"|d.CompanyName =="RSVN USSR"|d.CompanyName =="Starsem"|d.CompanyName =="VKS RF"|d.CompanyName =="Yuzhmash"){
+                d.Country = "Russia"
+            }else if (d.CompanyName =="Arianespace"){
+                d.Country = "Europe (ESA)"
+            }
         })
+
+        // console.log(rowID, csv[rowID])
+        // csv.splice(0,rowID)
+
+        //replace incorrect countries
+
 
         // different groupings
         // apparently you can group by multiple levels. Sweet!
         // let byRocketCategory = d3.group(csv, d => d.Rocket_Category);
         // let byCompany = d3.group(csv, d => d.CompanyName);
-        let byCompanyRocket = d3.group(csv, d => d.CountryName, d => d.CompanyName, d => d.Rocket_Category);
+        let byCompanyRocket = d3.group(csv, d => d.Country, d => d.CompanyName, d => d.Rocket_Category);
 
         // for some reason, byCompanyRocket outputs asynchronously as the finished product.
         // making an unused duplicate to compare/contrast final product
-        let byCountryCompanyRocket = d3.group(csv, d => d.CountryName, d => d.CompanyName, d => d.Rocket_Category);
+        let byCountryCompanyRocket = d3.group(csv, d => d.Country, d => d.CompanyName, d => d.Rocket_Category);
         // console.log(byCountryCompanyRocket)
 
 
@@ -51,7 +83,7 @@ function loadData() {
         let finalTree=[];
         finalTree = {name: "Rockets", children: finalArray}
 
-        console.log(finalTree)
+        // console.log(finalTree)
 
         let finalJSON = JSON.stringify(finalTree);
         console.log(finalJSON);
@@ -121,7 +153,7 @@ function summarizeData(data){
             // d is a map of all rockets per company
 
             // set up counters to collect data
-            var rocketName, rocketStatus, companyName, countryName;
+            var rocketName, rocketStatus, companyName, country;
             var rocketCounter = 0;
             var successCounter = 0;
             var failureCounter = 0;
@@ -136,7 +168,7 @@ function summarizeData(data){
                     rocketName = d.Rocket_Category;
                     rocketStatus = d.StatusRocket;
                     companyName = d.CompanyName;
-                    countryName = d.CountryName;
+                    countryName = d.Country;
                     rocketCounter++;
 
                     // count number of successes and failures per rocket type
